3754b0f68867b23c86e48f98f2558897117d9a65
RUBENSNETO0001 <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-10-24 15:52:34 -0500

    sdfdsf

--- att_02/js/script.js
+++ att_02/js/script.js
@@ -1,6 +1,13 @@
-let chavedeEntrada = document.getElementById("emailNome").value;
-let senha = document.getElementById("senha").value;
 
 function entrar(){
+    let chavedeEntrada = document.getElementById("emailNome").value;
+    let senha = document.getElementById("senha").value;
+    
+    if(chavedeEntrada == "" || senha == ""){
+        alert("Porfavor verifique se colocou algum no login");
+    }
+    else{
+        window.location = "../att_01/index.html";
+    }
 
 }
