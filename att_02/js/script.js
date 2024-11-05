dcefe1df98b447369ce657d60a6220b849c7293c
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2024-11-05 10:30:02 -0500

    versão 0.6.2

--- att_02/js/script.js
+++ att_02/js/script.js
@@ -1,4 +1,4 @@
-
+// usei uma função, que para amim e mais rapido ser executado
 function entrar(){
     let chavedeEntrada = document.getElementById("emailNome").value;
     let senha = document.getElementById("senha").value;
@@ -6,8 +6,13 @@ function entrar(){
     if(chavedeEntrada == "" || senha == ""){
         alert("Porfavor verifique se colocou algum no login");
     }
+    // botei umna chave padrao para entra no site, muito mais facil de entrar
+    else if(chavedeEntrada == "user" || senha == "1234"){
+         window.location = "../att_01/index.html";
+    }
     else{
-        window.location = "../att_01/index.html";
+        // caso o cara erre a senha. como não ta conectado a um banco de dados so tem uma senha possivel para acessar  o site.
+       alert("senha incorreta!!");
     }
 
 }
