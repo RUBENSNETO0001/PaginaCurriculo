5dce9fae78d28298f670384e671c4693f2136401
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-11-07 14:41:27 -0500

    versão:0.8.2

--- att_02/js/script.js
+++ att_02/js/script.js
@@ -1,19 +1,18 @@
-alert("senha de acesso:\nCHAVE DE ENTRADA: user\nSENHA: 1234")
+alert("Senha de acesso:\nCHAVE DE ENTRADA: user\nSENHA: 1234")
 // usei uma função, que para amim e mais rapido ser executado
-function entrar(){
+function entrar() {
     let chavedeEntrada = document.getElementById("emailNome").value;
     let senha = document.getElementById("senha").value;
-    
-    if(chavedeEntrada == "" || senha == ""){
-        alert("Porfavor verifique se colocou algum no login");
-    }
-    // botei umna chave padrao para entra no site, muito mais facil de entrar
-    else if(chavedeEntrada == "user" || senha == "1234"){
-         window.location = "../att_01/index.html";
-    }
-    else{
-        // caso o cara erre a senha. como não ta conectado a um banco de dados so tem uma senha possivel para acessar  o site.
-       alert("senha incorreta!!");
-    }
 
+        if (chavedeEntrada === "user" && senha === "1234") {
+            window.location.href = "../att_01/index.html";
+        }
+        else if(chavedeEntrada == "" || senha == ""){
+            alert("Verifique os campos não estão vazios");
+        }
+        else {
+            // caso o cara erre a senha. como não ta conectado a um banco de dados so tem uma senha possivel para acessar  o site.
+            alert("Porfavor verifique se colocou algum no campos do login!!");
+        }
+    
 }
